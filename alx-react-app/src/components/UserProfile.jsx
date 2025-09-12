ce88204be5dcf0418574ec400a1c6fc46bdf7f32
patrick_mugo <patrickkinyua2004@gmail.com>
2025-09-12 11:09:04 +0300

    ffff

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ const UserProfile = (props) => {
   return (
     <div>
       <h2>{props.name}</h2>
-      <p>Email: {props.Age}</p>
+      <p>Email: {props.age}</p>
       <p>Bio: {props.bio}</p>
     </div>
   )
